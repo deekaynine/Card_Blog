8b9b6ab430fd92b04421444ef2cd4f83b9413985
unknown <deekaynine@gmail.com>
2022-03-11 07:16:46 -0500

    added gitignore file

--- client/src/index.js
+++ client/src/index.js
@@ -1,5 +1,5 @@
 import React from "react";
-import ReactDom from "react-dom";
+import ReactDOM from "react-dom";
 import { Provider } from "react-redux";
 import { createStore, applyMiddleware, compose } from "redux";
 import thunk from "redux-thunk";
@@ -11,7 +11,7 @@ import "./index.css";
 
 const store = createStore(reducers, compose(applyMiddleware(thunk)));
 
-ReactDom.render(
+ReactDOM.render(
   <Provider store={store}>
     <App />
   </Provider>,
